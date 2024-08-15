67b9b036ecdedd4f352dd9a48f6bf8b7fd0591a5
David Escudero <davidescudero1023@gmail.com>
2024-08-14 18:33:27 -0600

    base

---
+++ mindpeace-backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Ruta de ejemplo
+app.get('/', (req, res) => {
+    res.send('Bienvenido a MindPeace API');
+});
+
+// Conectar a MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+   .then(() => console.log('Conectado a MongoDB'))
+   .catch(err => console.log('Error al conectar con MongoDB:', err));
+
+
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
