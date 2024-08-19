931b4c05fb8ba9ce342e87c35a26f288135fa689
ItsEliasMg <mayorelias12@gmail.com>
2024-08-18 18:18:41 -0600

    Se realizo el menu de mindpeace

--- mindpeace-frontend/src/App.js
+++ mindpeace-frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function App() {
     const [message, setMessage] = useState('');
@@ -15,8 +16,33 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <h1>{message}</h1>
+        <div className="menu-container">
+            <nav className="menu-nav">
+                <div className="menu-item">INICIO</div>
+                <div className="menu-item">
+                    <input type="text" placeholder="BUSCAR..." className="search-input" />
+                </div>
+                <div className="menu-item">EXPLORAR</div>
+            </nav>
+            <h1 className="menu-title">MINDPEACE</h1>
+            <div className="menu-icons">
+                <div className="icon-item">
+                    <img src="/icons/tutorial.png" alt="Tutorial" />
+                    <p>TUTORIALES<br /><span>Acerca de MindPeace</span></p>
+                </div>
+                <div className="icon-item">
+                    <img src="/icons/confi.png" alt="Configuración" />
+                    <p>CONFIGURACIÓN<br /><span>Perfil</span></p>
+                </div>
+                <div className="icon-item">
+                    <img src="/icons/test.png" alt="Test de Ansiedad" />
+                    <p>TEST<br /><span>De ansiedad</span></p>
+                </div>
+                <div className="icon-item">
+                    <img src="/icons/form.png" alt="Formulario MindPeace" />
+                    <p>FORMULARIO<br /><span>MindPeace</span></p>
+                </div>
+            </div>
         </div>
     );
 }
